1e56d6f54b3458a1ceea09c63e7190d2b0720c07
SpringBoots <13266699268@qq.com>
2017-05-05 18:59:01 +0800

    init baseApp

---
+++ app/src/main/java/com/android/base/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.base;
+
+
+import com.alphabet.app.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+    
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
